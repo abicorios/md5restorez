3343228b9b941f31c9da6a7b851315c36673d549
abicorios <yaroslav.moroz91@gmail.com>
2018-08-27 12:20:17 -0700

    fix subprocess

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
-call 2make.bat
-copy app.html md5scannerz\app /y
-copy app.js md5scannerz\src /y
-if not exist md5scannerz\resources\app mkdir md5scannerz\resources\app
-copy md5scannerzgo.exe md5scannerz\resources\app /y
-copy md5scannerzgo.exe md5scannerz /y
-copy package.json md5scannerz\package.json /y
-xcopy 7z1805-extra md5scannerz\7z1805-extra\ /s /y
-xcopy 7z1805-extra md5scannerz\resources\app\7z1805-extra\ /s /y
-start cmd /c "cd md5scannerz && npm start"
+call make.bat
+copy app.html md5restorez\app /y
+copy app.js md5restorez\src /y
+if not exist md5restorez\resources\app mkdir md5restorez\resources\app
+copy md5restorezPyEmbeddable.exe md5restorez\resources\app /y
+copy md5restorezPyEmbeddable.exe md5restorez /y
+copy package.json md5restorez\package.json /y
+xcopy 7z1805-extra md5restorez\7z1805-extra\ /s /y
+xcopy 7z1805-extra md5restorez\resources\app\7z1805-extra\ /s /y
+start cmd /c "cd md5restorez && npm start"
