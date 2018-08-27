92e7c2702868b7f3a5c50ce79374f921f07c1944
abicorios <yaroslav.moroz91@gmail.com>
2018-08-27 08:53:28 -0700

    comment run date

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 call 0installTools.bat
 if not exist md5restorez start /wait cmd /c "git clone https://github.com/szwacz/electron-boilerplate.git md5restorez && cd md5restorez && npm install"
-start /wait cmd /c ruby date.rb
+rem start /wait cmd /c ruby date.rb
 if not exist md5restorezPyEmbeddable start /wait cmd /c git clone https://github.com/abicorios/md5restorezPyEmbeddable
