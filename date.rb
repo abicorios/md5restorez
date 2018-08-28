5357798d6678494cb4ad767bfbde5fe5ff4044b8
abicorios <yaroslav.moroz91@gmail.com>
2018-08-28 04:38:06 -0700

    fix update

--- date.rb
+++ date.rb
@@ -1,9 +1,12 @@
 require 'date'
 f=File.read("package.json")
+name=f[/(?<="name": ")\w+/]
 regv=/(\d+\.){2}\d+/
 v=f[/version.*\n/][regv]
 source=File.read("app.html")
-source2=source.gsub(/(?<=v)(\d+\.){2}\d+/,v)
+source.sub!(/(?<=<title>)\w+/,name)
+source.sub!(/(?<=<p>)(\w+)(?= ([^\.]{1,4}\.){4})/,name)
+source.sub!(/(?<=v)(\d+\.){2}\d+/,v)
 date=Date.today.strftime("%d.%m.%Y")
-source3=source2.gsub(/(\d{2}\.){2}\d{4}/,date)
-File.open("app.html","w") {|f| f.puts source3}
+source.sub!(/(\d{2}\.){2}\d{4}/,date)
+File.open("app.html","w") {|f| f.puts source}
--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 call 0installTools.bat
 if not exist md5restorez start /wait cmd /c "git clone https://github.com/szwacz/electron-boilerplate.git md5restorez && cd md5restorez && npm install"
-rem start /wait cmd /c ruby date.rb
+start /wait cmd /c ruby date.rb
 if not exist md5restorezPyEmbeddable start /wait cmd /c git clone https://github.com/abicorios/md5restorezPyEmbeddable
 start /wait cmd /c "cd md5restorezPyEmbeddable && git pull"
 if not exist md5restorezPyEmbeddable.exe call compileMd5restorezPyEmbeddable.bat
