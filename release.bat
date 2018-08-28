274ec8ccfaff46af4db0b51b9f1b77f82d887755
abicorios <yaroslav.moroz91@gmail.com>
2018-08-28 13:42:52 -0700

    add background img

--- release.bat
+++ release.bat
@@ -5,5 +5,6 @@ if not exist md5restorez\resources\app mkdir md5restorez\resources\app
 copy package.json md5restorez\package.json /y
 if not exist md5restorez\7z1805-extra xcopy 7z1805-extra md5restorez\7z1805-extra\ /s /y
 if not exist md5restorez\resources\app\7z1805-extra\ xcopy 7z1805-extra md5restorez\resources\app\7z1805-extra\ /s /y
+copy mario-3631666_1280.png md5restorez\app /y
 start /wait cmd /c "cd md5restorez && npm run release"
 copy  md5restorez\dist\md5restorez* /y
--- start.bat
+++ start.bat
@@ -7,4 +7,5 @@ copy md5restorezPyEmbeddable.exe md5restorez /y
 copy package.json md5restorez\package.json /y
 if not exist md5restorez\7z1805-extra xcopy 7z1805-extra md5restorez\7z1805-extra\ /s /y
 if not exist md5restorez\resources\app\7z1805-extra\ xcopy 7z1805-extra md5restorez\resources\app\7z1805-extra\ /s /y
+copy mario-3631666_1280.png md5restorez\app /y
 start cmd /c "cd md5restorez && npm start"
