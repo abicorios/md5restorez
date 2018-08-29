c631c5d73c864c65ff6ef7c5cea7e99420ae3ccd
abicorios <yaroslav.moroz91@gmail.com>
2018-08-29 07:25:40 -0700

    clear old tests and optimization

--- make.bat
+++ make.bat
@@ -7,3 +7,13 @@ if not exist md5restorezPyEmbeddable.exe call compileMd5restorezPyEmbeddable.bat
 compileIfChanged.exe md5restorezPyEmbeddable\md5restorezPyEmbeddable.py md5restorezPyEmbeddable.exe compileMd5restorezPyEmbeddable.bat
 if not exist 7z1805-extra (git clone https://github.com/abicorios/7zGitMirror
 move 7zGitMirror\7z1805-extra)
+copy app.html md5restorez\app /y
+copy app.js md5restorez\src /y
+if not exist md5restorez\resources\app mkdir md5restorez\resources\app
+copy md5restorezPyEmbeddable.exe md5restorez\resources\app /y
+copy md5restorezPyEmbeddable.exe md5restorez /y
+copy package.json md5restorez\package.json /y
+if not exist md5restorez\7z1805-extra xcopy 7z1805-extra md5restorez\7z1805-extra\ /s /y
+if not exist md5restorez\resources\app\7z1805-extra\ xcopy 7z1805-extra md5restorez\resources\app\7z1805-extra\ /s /y
+copy nintendo_x_sega_3_reborn__wallpaper_by_thegamerlover-db9kgj3.jpg md5restorez\app /y
+copy background.js md5restorez\src /y
--- start.bat
+++ start.bat
@@ -1,11 +1,2 @@
 call make.bat
-copy app.html md5restorez\app /y
-copy app.js md5restorez\src /y
-if not exist md5restorez\resources\app mkdir md5restorez\resources\app
-copy md5restorezPyEmbeddable.exe md5restorez\resources\app /y
-copy md5restorezPyEmbeddable.exe md5restorez /y
-copy package.json md5restorez\package.json /y
-if not exist md5restorez\7z1805-extra xcopy 7z1805-extra md5restorez\7z1805-extra\ /s /y
-if not exist md5restorez\resources\app\7z1805-extra\ xcopy 7z1805-extra md5restorez\resources\app\7z1805-extra\ /s /y
-copy nintendo_x_sega_3_reborn__wallpaper_by_thegamerlover-db9kgj3.jpg md5restorez\app /y
 start cmd /c "cd md5restorez && npm start"
